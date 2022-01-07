1376c4c00e7974fba177caf8fc6474b0459e82cb
Robin Phillips <robinlmp@mac.com>
2022-01-07 19:34:46 +0000

    Challenge complete

--- Challenge2/ViewController.swift
+++ Challenge2/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForItem))
+        
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Item")
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        print(indexPath)
+        return cell
+    }
+    
+    
+    @objc func promptForItem() {
+        let ac = UIAlertController(title: "Enter item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
 
+        let submitAction = UIAlertAction(title: "Add", style: .default) { [weak self, weak ac] action in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.addShoppingItem(item)
+        }
 
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func addShoppingItem(_ item: String) {
+        shoppingList.insert(item, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
 }
 
